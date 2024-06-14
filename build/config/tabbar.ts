4f63a54c373a0885b3cf5697a8c6ba8229f6ff61
s-xianyu <s22539634@aliyun.com>
2024-06-14 16:25:01 +0800

    feat: 替换成网络图片

--- build/config/tabbar.ts
+++ build/config/tabbar.ts
@@ -1,20 +1,20 @@
 export default [
   {
     pagePath: 'pages/tab/home/index',
-    iconPath: '/static/images/home.png',
-    selectedIconPath: '/static/images/home-active.png',
+    iconPath: 'https://s21.ax1x.com/2024/06/14/pkdBqDP.png',
+    selectedIconPath: 'https://s21.ax1x.com/2024/06/14/pkdBbut.png',
     text: '首页',
     customIcon: false
   }, {
     pagePath: 'pages/tab/list/index',
-    iconPath: '/static/images/home.png',
-    selectedIconPath: '/static/images/home-active.png',
+    iconPath: 'https://s21.ax1x.com/2024/06/14/pkdBqDP.png',
+    selectedIconPath: 'https://s21.ax1x.com/2024/06/14/pkdBbut.png',
     text: '列表',
     customIcon: false
   }, {
     pagePath: 'pages/tab/user/index',
-    iconPath: '/static/images/home.png',
-    selectedIconPath: '/static/images/home-active.png',
+    iconPath: 'https://s21.ax1x.com/2024/06/14/pkdBqDP.png',
+    selectedIconPath: 'https://s21.ax1x.com/2024/06/14/pkdBbut.png',
     text: '个人中心',
     customIcon: false
   }
